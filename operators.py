0dbcbfb24364f346281c60544e9fe27125140cb7
Asif6 <asiffoysal12@gmail.com>
2021-10-15 16:52:24 +0600

    Add operators.py ==>>

---
+++ operators.py
@@ -0,0 +1,29 @@
+# Add or + operator 
+a=10
+b=5
+
+print(f"result=: {a+b}")
+
+# Sub or - operator
+print(f"The result is : {a-b}")
+
+# Multiplication or * operator
+print(f"The result is {a*b}")
+print(f"The result is {a**b}") # a^b We use ** for Power Calculations
+# Division or / 
+print(f"The result is {a/b}")
+print(f"The result is {123//10}") # Floor Division
+
+# Modulus or % Operator
+
+print(f"The result is {a%b}")
+
+# Assignment operator or =
+a=10
+print(f"The result is {a}")
+a+=10
+print(f"The result is {a}")
+a-=5
+a*=2
+a/=4
+a%=2
