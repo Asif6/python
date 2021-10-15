232b19fc10aef3f865c3425024f2b7c438e29f9b
Asif6 <asiffoysal12@gmail.com>
2021-10-15 18:03:37 +0600

    Update operators.py

--- operators.py
+++ operators.py
@@ -27,3 +27,68 @@ a-=5
 a*=2
 a/=4
 a%=2
+
+# And operator 
+a=10;b=20
+if a==10 and b==20:
+    print("OK")
+else:
+    print("Not Ok")
+# Or operator (or)
+
+if a==10 or b==20:
+    print("OK")
+else:
+    print("Not OK")
+
+# Not operator not
+
+if a !=20:
+    print("Ok")
+else:
+    print("Not OK")
+if not a:
+    print("YES")
+else:
+    print("NO")
+
+# Greater Than  >
+result=a>b
+print(result)
+
+# Less Than <
+
+result=a<b
+print(result)
+
+# Greater Than or Equal To	>=
+result=a>=b
+print(result)
+
+# Less Than or Equal To <=
+result=a<=b
+print(result)
+
+# Equal To ==
+result=a==b
+print(result)
+
+# Not Equal To !=
+result=a!=b
+print(result)
+
+# Python Identity Operators is/is not 
+
+a is b  #Returns True if both variables are the same object
+x = ["apple", "banana"]
+print("banana" in x)
+
+a is not b #Returns True if both variables are not the same object
+
+x = ["apple", "banana"]
+
+print("pineapple" not in x)
+
+# returns True because a sequence with the value "pineapple" is not in the list
+
+
