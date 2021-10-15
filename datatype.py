fbd5ec59bcc99563f64c0b4a8e6a9cd6005cb5bc
Asif6 <asiffoysal12@gmail.com>
2021-10-15 15:35:26 +0600

    Add a File Called datatype.py ==>>

---
+++ datatype.py
@@ -0,0 +1,18 @@
+# Integer data type
+
+valu=100
+print(valu)
+
+# Float data type 
+valu=100.11
+print(valu)
+
+# Boolean data type
+
+valu=True
+valu2=False
+print(f"True valu: {valu} False valu: {valu2}")
+
+# String in python 
+valu="Asif"
+print(valu)
