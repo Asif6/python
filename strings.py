ad298536f85ac292651abdafda07b0902bc521ec
Asif6 <asiffoysal12@gmail.com>
2021-10-15 16:14:25 +0600

    Add string.py ==>> Here All string slicing formate r written

---
+++ strings.py
@@ -0,0 +1,10 @@
+# string slicing in python 
+
+valu="Asif Foysal"
+
+print(valu[::-1]) # Revirce the string
+print(valu[4::]) # Start  index 5 to end
+print(valu[:4]) # Up to index 4 non include index 4
+print(valu[::2]) # Stap sige 2 that min it delet a charecter after every 2 charecter 
+print (valu[-6::]) # Last 6 char of a string
+print(valu[-1]) # Last charecter of a string 
